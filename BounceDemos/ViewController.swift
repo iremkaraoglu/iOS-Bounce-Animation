912a8aa5c45287182184001b16b478a952803169
iremkaraoglu <iremk97@gmail.com>
2022-06-26 13:02:40 +0300

    initial commit

--- BounceDemos/ViewController.swift
+++ BounceDemos/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
+        addDot()
     }
 
 
+    func addDot() {
+        let ball = UIView()
+        ball.frame = CGRect(x: view.center.x, y: view.center.y, width:  UIDevice.current.userInterfaceIdiom == .pad ? 18 : 12, height:  UIDevice.current.userInterfaceIdiom == .pad ? 18 : 12)
+        ball.layer.cornerRadius = UIDevice.current.userInterfaceIdiom == .pad ? 9 : 6
+        ball.backgroundColor = .white
+        view.addSubview(ball)
+    }
+    
 }
 
