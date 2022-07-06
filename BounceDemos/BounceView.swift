6e4cac4225fa4a2a27bbd8299c2d7f25b53a9fcd
iremkaraoglu <iremk97@gmail.com>
2022-07-06 22:57:20 +0300

    update bezierpath and add play button

--- BounceDemos/BounceView.swift
+++ BounceDemos/BounceView.swift
@@ -20,6 +20,7 @@ class BounceView : UIView {
     var dotYvalue = 48.0
     var points = [CGPoint]()
     var durations = [CGFloat]()
+    var initialPoint = CGPoint()
     var dot: UIView?
     
     init(frame: CGRect, dotSize: CGFloat, labels: [UILabel]) {
@@ -30,13 +31,36 @@ class BounceView : UIView {
         durations.append(0.3)
         durations.append(0.3)
         super.init(frame: frame)
+        addPlayButton()
+        
+        createRow()
         addDot()
-        startDotAnimation()
+        //        startDotAnimation()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    
+    func addPlayButton() {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Play", for: .normal)
+        button.backgroundColor = .gray
+        self.addSubview(button)
+        NSLayoutConstraint.activate([
+            button.widthAnchor.constraint(equalToConstant: 48),
+            button.heightAnchor.constraint(equalToConstant: 48),
+            button.topAnchor.constraint(equalTo: self.topAnchor)
+        ])
+        button.addTarget(self, action: #selector(onPress), for: .touchUpInside)
+    }
+    
+    @objc func onPress() {
+        startDotAnimation()
+    }
+    
     
     func addDot() {
         let ball = UIView()
@@ -49,9 +73,58 @@ class BounceView : UIView {
         NSLayoutConstraint.activate([
             ball.widthAnchor.constraint(equalToConstant: dotSize),
             ball.heightAnchor.constraint(equalToConstant: dotSize),
-            ball.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 50),
-            ball.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            ball.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: initialPoint.x),
+            ball.topAnchor.constraint(equalTo: self.topAnchor, constant: initialPoint.y),
         ])
+        
+        
+    }
+    
+    func createBezierPath() -> UIBezierPath? {
+        var count = 0
+        let path = UIBezierPath()
+        var point = CGPoint(x: startingPoint.x - 8  , y: 70)
+        var previousLabelWidth = 10.0
+        var deltaX = 0.0
+        
+        for i in labels {
+            if labelWidths.isEmpty {
+                break
+            }
+            
+            deltaX = (previousLabelWidth  + (8 * 2) + labelWidths[count])/2
+            
+            if count == 0 {
+                path.move(to: point)
+            }
+            
+            let controlPoint1 = CGPoint(x: point.x + deltaX/2,
+                                        y: point.y - dotYvalue)
+            let controlPoint2 = CGPoint(x: point.x + deltaX/2,
+                                        y: point.y - dotYvalue)
+            let destination = CGPoint(x: point.x + deltaX,
+                                      y: point.y)
+            point = destination
+            points.append(destination)
+            path.addCurve(to: destination, controlPoint1: controlPoint1, controlPoint2: controlPoint2)
+            previousLabelWidth = labelWidths[count]
+            durations.append(0.3)
+            count += 1
+        }
+        
+        let controlPoint1 = CGPoint(x:  point.x + 20,
+                                    y: point.y - dotYvalue)
+        let controlPoint2 = CGPoint(x:  point.x + 20,
+                                    y: point.y - dotYvalue)
+        let destination = CGPoint(x:  point.x + 40,
+                                  y: point.y)
+        point = destination
+        points.append(destination)
+        path.addCurve(to: destination, controlPoint1: controlPoint1, controlPoint2: controlPoint2)
+        durations.append(0.3)
+        
+        return path
+        
     }
     
     func startDotAnimation() {
@@ -68,7 +141,6 @@ class BounceView : UIView {
         var index = 0
         for d in durations {
             cd = cd + d
-            // bu çarpanı değiştirebilirsin
             let xd = cd - d*0.5
             var percent = xd / animation.duration as NSNumber
             if index + 1 == durations.count{
@@ -77,48 +149,23 @@ class BounceView : UIView {
             keyTimes.append(percent)
             index += 1
         }
-//        keyTimes.append(1.0)
         
         animation.keyTimes = keyTimes
         self.dot?.layer.add(animation, forKey: "bezier")
         self.dot?.alpha = 1
         self.dot?.center = self.points.last!
+        
+        //        self.delay(seconds: 0.1) {
+        //            self.dot.layer.zPosition = 999
+        //            self.bringSubviewToFront(self.dot)
+        //            self.dot.alpha = 1
+        //        }
+        //
+        //        self.delay(seconds: animation.duration - 0.3) {
+        //            self.dot.alpha = 0
+        //        }
     }
     
-    
-    func createBezierPath() -> UIBezierPath? {
-        var count = 0
-        let path = UIBezierPath()
-        var point = CGPoint(x: 50, y: self.center.y)
-        var previousLabelWidth = 10.0
-        var deltaX = 0.0
-        
-        for i in self.labels {
-            if count >= self.labels.count {
-                break
-            }
-//            deltaX = (previousLabelWidth  + (8 * 2) + labelWidths[count])/2
-            deltaX = 40
-            
-            if count == 0 {
-                path.move(to: point)
-            }
-            
-            let controlPoint1 = CGPoint(x: point.x + deltaX/2,
-                                        y: point.y - dotYvalue)
-            let controlPoint2 = CGPoint(x: point.x + deltaX/2,
-                                        y: point.y - dotYvalue)
-            let destination = CGPoint(x: point.x + deltaX,
-                                      y: point.y)
-            point = destination
-            points.append(destination)
-            path.addCurve(to: destination, controlPoint1: controlPoint1, controlPoint2: controlPoint2)
-//            previousLabelWidth = labelWidths[count]
-            count += 1
-        }
-        return path
-        
-    }
     
     
     
@@ -145,10 +192,12 @@ class BounceView : UIView {
             }
             count += 1
         }
-        let parentWidth = self.frame.width
-        let topLeft: CGPoint = .init(x: (parentWidth - rowWidth)/2, y: 80)
+        let parentWidth = UIScreen.main.bounds.width
+        let topLeft: CGPoint = .init(x: (parentWidth - rowWidth)/2, y: self.center.y - 20)
+        initialPoint.x = (parentWidth - rowWidth)/2 - labelWidths[0]/2
+        initialPoint.y = self.center.y - 20
         positionLabels(rowLabels: row,  labelWidths: labelWidths, rowStartPoint: topLeft)
-
+        
     }
     
     func positionLabels(rowLabels: [UILabel], labelWidths: [CGFloat], rowStartPoint: CGPoint) {
@@ -160,6 +209,7 @@ class BounceView : UIView {
             l.frame = CGRect(x: deltaX, y: rowStartPoint.y , width: labelWidths[count], height: 20)
             deltaX = deltaX + 8 + labelWidths[count]
             count += 1
+            self.addSubview(l)
         }
     }
     
--- BounceDemos/ViewController.swift
+++ BounceDemos/ViewController.swift
@@ -15,30 +15,32 @@ class ViewController: UIViewController {
         addBounceView()
     }
     
+
+    
     func addBounceView() {
         let label = UILabel()
         label.text = "Lorem"
-        
+
         let label1 = UILabel()
         label1.text = "Ipsum"
-        
+
         let label2 = UILabel()
         label2.text = "Dolor"
-        
+
         let label3 = UILabel()
         label3.text = "Sit"
-        
+
         let label4 = UILabel()
         label4.text = "Amet"
-        
-        
+
+
         var labelArray = [UILabel]()
         labelArray.append(label)
         labelArray.append(label1)
         labelArray.append(label2)
         labelArray.append(label3)
         labelArray.append(label4)
-        let bounceView = BounceView(frame: view.frame, dotSize: 12, labels: labelArray)
+        let bounceView = BounceView(frame: CGRect(x:0, y:view.center.y, width: UIScreen.main.bounds.width, height: 150), dotSize: 12, labels: labelArray)
         view.addSubview(bounceView)
     }
 }
